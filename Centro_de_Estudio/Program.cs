e5daec0679af62fb96b67917059a363813b1ccdc
Legolas <157653932+Legolasfk@users.noreply.github.com>
2025-11-02 16:03:17 -0600

    fead
    sd

--- Centro_de_Estudio/Program.cs
+++ Centro_de_Estudio/Program.cs
@@ -18,6 +18,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=MenuPrincipalProfesores}/{action=MPProfesores}/{id?}");
+    pattern: "{controller=MenuPrincipal}/{action=MenuPrincipal}/{id?}");
 
 app.Run();
